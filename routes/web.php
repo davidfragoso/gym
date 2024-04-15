b8ca8bfe71e0586ab8eeff908eb026a45bf92399
davidfragoso <fragosomadera99@gmail.com>
2024-04-15 11:06:23 -0500

    configurando entorno api

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,16 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\SysAdminController;
 use App\Http\Controllers\EmailController;
 
-Route::get('/users', [UserController::class, 'index']);
-
-// Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::post('/register', [UserController::class, 'register']);
-// });
+});
 
-// Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::post('/admin/register', [SysAdminController::class, 'register']);
-// });
+});
 
 // Rutas para el controlador de correos electrónicos
-// Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::post('/send-emails', [EmailController::class, 'sendEmailsToUsersAndAdmins']);
-// });
+});
 
