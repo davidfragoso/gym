fe2207ee985354b41dc3aad697608fe285e361c5
davidfragoso <fragosomadera99@gmail.com>
2024-04-15 02:11:53 -0500

    second commit

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,18 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\SysAdminController;
 use App\Http\Controllers\EmailController;
 
-Route::middleware(['auth'])->group(function () {
-    Route::post('/register', [UserController::class, 'register']);
-});
+Route::get('/users', [UserController::class, 'index']);
 
-Route::middleware(['auth'])->group(function () {
+// Route::middleware(['auth'])->group(function () {
+    Route::post('/register', [UserController::class, 'register']);
+// });
+
+// Route::middleware(['auth'])->group(function () {
     Route::post('/admin/register', [SysAdminController::class, 'register']);
-});
+// });
 
 // Rutas para el controlador de correos electrónicos
-Route::middleware(['auth'])->group(function () {
+// Route::middleware(['auth'])->group(function () {
     Route::post('/send-emails', [EmailController::class, 'sendEmailsToUsersAndAdmins']);
-});
+// });
 
